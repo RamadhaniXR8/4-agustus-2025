b2792ff8297d02fa512147da3847d5ab839fd71e
Ramadhani <ramadhani_pradana_34rpl@student.smktelkom-mlg.sch.id>
2025-08-04 13:16:11 +0700

    Add files via upload

--- ini.java
+++ ini.java
@@ -1,5 +1,10 @@
 public class ini {
     public static void main(String[] args) {
         System.out.println("Ramadhani");
+        System.out.println("SMPI Excellent As Syafiah");
+        System.out.println("Damijan");
+        System.out.println("Endah Paramita");
+        System.out.println("Bekerja Di Jepang");
+        System.out.println("Bermain Game");
     }
 }
