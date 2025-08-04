2d26b2f7d78bd46a6a1f968b3f4cb883b1dbcfbf
Ramadhani <ramadhani_pradana_34rpl@student.smktelkom-mlg.sch.id>
2025-08-04 14:22:22 +0700

    Add files via upload

---
+++ teman.java
@@ -0,0 +1,30 @@
+public class teman {
+    public static void main(String[] args) {
+        //variabel
+    String nama="ilham Daniswara ";
+    int usia=15;
+    String cita=" software enginer";
+    System.out.println(nama+usia+cita);
+
+    String nama1="Iqbal Ibrahim ";
+    int usia1=15;
+    String cita1=" pembisnis";
+    System.out.println(nama1+usia1+cita1);
+
+    String nama2="Hisyam Darmawan ";
+    int usia2=14;
+    String cita2=" Pembalap";
+    System.out.println(nama2+usia2+cita2);
+
+    String nama3="Raditya Prima ";
+    int usia3=16;
+    String cita3=" Polisi";
+    System.out.println(nama3+usia3+cita3);
+
+    String nama4="Maven Helios ";
+    int usia4=15;
+    String cita4=" Web Developer";
+    System.out.println(nama4+usia4+cita4);
+    }
+    
+}
