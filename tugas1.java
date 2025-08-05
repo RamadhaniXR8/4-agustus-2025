bd82f058569ea49c1882c87371947c13b637d07c
Ramadhani <ramadhani_pradana_34rpl@student.smktelkom-mlg.sch.id>
2025-08-05 17:08:24 +0700

    Add files via upload

---
+++ tugas1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class tugas1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Barang1");
+        String Barang1 = input.nextLine();
+        System.out.println("Harga Barang1");
+        int HargaBarang1 = input.nextInt();
+
+        System.out.println("Barang2");
+        String Barang2 = input.next();
+        System.out.println("Harga Barang2");
+        int HargaBarang2 = input.nextInt();
+
+        System.out.println("===================================================");
+    }
+}
